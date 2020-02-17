b89e33eff3c63b721639696fd0165ecf75cbf247
ali abdalla <ali.abdalla@outlook.fr>
2020-02-17 17:07:07 +0100

    vue -js

---
+++ tutorial/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div >
+    <mois />
+  </div>
+</template>
+
+<script>
+import Mois from './components/Mois.vue'
+
+export default {
+ 
+  components: {
+    Mois
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ tutorial/src/components/Mois.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+    <div v-for="m in mois" :key="m.mois">{{ m.mois }}
+      
+           : {{m.days}}
+        
+    </div>
+    
+</div>
+</template>
+<script>
+export default {
+    name:"Mois", 
+    data: ()=>{
+        return {
+            mois: Mois
+
+        };
+    }
+    
+};
+const Mois=[
+    { mois: 'Janvier', days: 1},
+    { mois : 'Fervrier', days: 2},
+    { mois : 'Mars', days :3},
+    { mois : 'Avril', days: 4 },
+    { mois : 'Mai', days: 5},
+    { mois : 'Juin', days:6},
+    { mois : 'Juillet', days:7 },
+    { mois : 'Août', days:8},
+    { mois : 'Septembre', days: 9},
+    { mois : 'Octobre', days: 10},
+    { mois : 'Novembre' ,days :11},
+    { mois : 'Décembre' ,days: 12}
+];
+
+
+
+</script>>
+
+
+<style  scoped>
+
+</style>
